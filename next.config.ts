ca7f9a4e5a37abe82ce5ef3553456d97f13bbbec
peppecaru00 <carusothe99@gmail.com>
2026-02-02 10:25:33 +0100

    adad

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio_v3' : '',
-
+  output: 'export',
 };
 
 export default nextConfig;
