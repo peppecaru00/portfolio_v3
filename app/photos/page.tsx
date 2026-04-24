8ae77cbe54d645a5210e6785159622da5f721e3d
peppecaru00 <carusothe99@gmail.com>
2026-04-24 17:18:32 +0200

    fixed vercel and photos scale grid

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -30,7 +30,7 @@ export default async function PhotosPage() {
       </div>
 
       {/* Projects Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-9 gap-9">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {projects.map((project) => (
           <Link
             key={project.id}
